7aefd5abe9e2856cc9c60760ee623691159b27bd
xiedong11 <xiedong11@aliyun.com>
2018-01-22 11:12:09 +0800

    引入glide结合data binding加载图片

--- qyb/src/main/java/com/zhuandian/qxe/base/binding/ImageBindingAdapter.java
+++ qyb/src/main/java/com/zhuandian/qxe/base/binding/ImageBindingAdapter.java
@@ -2,7 +2,10 @@ package com.zhuandian.qxe.base.binding;
 
 import android.databinding.BindingAdapter;
 import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
 import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 /**
  * desc：图片控件绑定的适配器
@@ -10,8 +13,11 @@ import android.widget.ImageView;
  * date：2018/1/22.
  */
 public class ImageBindingAdapter {
-    @BindingAdapter(value = {"url","placeHolder"} ,requireAll = false)
-    public static void loadImage(ImageView view, String url , Drawable placeholderImageRes){
-
+    @BindingAdapter(value = {"url", "placeHolder"}, requireAll = false)
+    public static void loadImage(ImageView view, String url, Drawable placeholderImageRes) {
+        if (TextUtils.isEmpty(url)) {
+            return;
+        }
+        Glide.with(view.getContext()).load(url).dontAnimate().placeholder(placeholderImageRes).into(view);
     }
 }
