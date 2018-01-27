a9616c63de60faa49bb9541c8286eca6ec455b2e
songbl <791243928@qq.com>
2018-01-27 13:21:23 +0800

    test commit (#9)
     测试提交

--- qyb/src/main/java/com/zhuandian/qxe/ExtralModule/wkzxs/ZHLActivity.java
+++ qyb/src/main/java/com/zhuandian/qxe/ExtralModule/wkzxs/ZHLActivity.java
@@ -6,44 +6,44 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.zhuandian.qxe.R;
+import com.zhuandian.qxe.base.QYBActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by 谢栋 on 2016/10/15.
  */
-public class ZHLActivity extends ActionBarActivity {
+public class ZHLActivity extends QYBActivity {
+
+    @BindView(R.id.toolbar_title)
+    TextView toolbarTitle;
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.zhl_wklist);
+    public int getLayoutId() {
+        return R.layout.zhl_wklist;
+    }
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        ((TextView)findViewById(R.id.toolbar_title)).setText("综合楼");
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    @Override
+    public void setupView() {
+        toolbarTitle.setText("文史楼");
+    }
 
+    @Override
+    public void setModle() {
 
     }
 
 
-    /**
-     * 菜单监听事件
-     * @param item
-     * @return
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-//        左上角返回菜单的键值为android.R.id.home
-        // TODO Auto-generated method stub
-        if(item.getItemId() == android.R.id.home)
-        {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    @OnClick(R.id.iv_back)
+    public void onViewClicked() {
+        finish();
     }
 }
