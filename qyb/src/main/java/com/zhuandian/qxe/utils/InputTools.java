2c43f11fa575736ab269472a26e7b9574981e192
xiedong11 <zx381010>
2018-02-01 09:30:46 +0800

    添加判断虚拟键盘显示与隐藏的工具类

---
+++ qyb/src/main/java/com/zhuandian/qxe/utils/InputTools.java
@@ -0,0 +1,101 @@
+package com.zhuandian.qxe.utils;
+
+/**
+ * desc：虚拟键盘控制类
+ * author：谢栋
+ * date：2018-02-01
+ */
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class InputTools {
+
+    //隐藏虚拟键盘
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm.isActive()) {
+            imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
+        }
+    }
+
+    //隐藏虚拟键盘
+    public static void hideKeyboard(View v) {
+        InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm.isActive()) {
+            imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+        }
+    }
+
+    //显示虚拟键盘
+    public static void showKeyboard(View v) {
+        InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        imm.showSoftInput(v, InputMethodManager.SHOW_FORCED);
+
+    }
+
+    //强制显示或者关闭系统键盘
+    public static void autoKeyBoard(final EditText txtSearchKey, final boolean status) {
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                InputMethodManager m = (InputMethodManager)
+                        txtSearchKey.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (status) {
+                    m.showSoftInput(txtSearchKey, InputMethodManager.SHOW_FORCED);
+                } else {
+                    m.hideSoftInputFromWindow(txtSearchKey.getWindowToken(), 0);
+                }
+            }
+        }, 300);
+    }
+
+    //通过定时器强制隐藏虚拟键盘
+    public static void timerHideKeyboard(final View v) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm.isActive()) {
+                    imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+                }
+            }
+        }, 10);
+    }
+
+    //输入法是否显示着
+    public static boolean isShow(EditText edittext) {
+        boolean bool = false;
+        InputMethodManager imm = (InputMethodManager) edittext.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm.isActive()) {
+            bool = true;
+        }
+        return bool;
+
+    }
+
+    /**
+     * 判断软键盘是否弹出
+     */
+    public static boolean isShowKeyboard(Context context, View v) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm.hideSoftInputFromWindow(v.getWindowToken(), 0)) {
+            imm.showSoftInput(v, 0);
+            //软键盘已弹出
+            return true;
+        } else {
+            //软键盘未弹出
+            return false;
+        }
+    }
+}
